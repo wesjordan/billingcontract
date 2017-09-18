8f287d4b9415dd4451a39839103d76973d6da94d
wesjordan <jordan.wesley@gmail.com>
2017-09-18 20:42:21 +0200

    include db schema

--- src/main/resources/db/migration/V1__initial_setup.sql
+++ src/main/resources/db/migration/V1__initial_setup.sql
@@ -2,9 +2,9 @@ DROP DATABASE IF EXISTS `billing_contract`;
 
 CREATE DATABASE `billing_contract`;
 
-DROP TABLE IF EXISTS `product_a`;
+DROP TABLE IF EXISTS `billing_contract`.`product_a`;
 
-CREATE TABLE `product_a` (
+CREATE TABLE `billing_contract`.`product_a` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `account_id` INT(8),
   `charge` DECIMAL(10,2) NULL,
