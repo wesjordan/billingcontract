e0ff941f9f1c150ececdcc84df56f1029354dae3
wesjordan <jordan.wesley@gmail.com>
2017-09-27 21:24:52 +0200

    ++ swagger

--- src/main/java/com/wesjordan/billingcontract/BillingcontractApplication.java
+++ src/main/java/com/wesjordan/billingcontract/BillingcontractApplication.java
@@ -2,11 +2,28 @@ package com.wesjordan.billingcontract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BillingcontractApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BillingcontractApplication.class, args);
+	}
+
+	@Bean
+	public Docket api(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.regex("/productA.*"))
+				.build();
+
 	}
 }
