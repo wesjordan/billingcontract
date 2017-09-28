9be030ff62cdc63c2eebcdef9abc989372b46c79
wesjordan <jordan.wesley@gmail.com>
2017-09-28 22:30:51 +0200

    - added test for add ProductA scenario - added @after method for test teardowns

--- src/test/java/com/wesjordan/billingcontract/controller/ProductAControllerTest.java
+++ src/test/java/com/wesjordan/billingcontract/controller/ProductAControllerTest.java
@@ -1,25 +1,28 @@
 package com.wesjordan.billingcontract.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wesjordan.billingcontract.BillingcontractApplication;
 import com.wesjordan.billingcontract.domain.ProductA;
 import com.wesjordan.billingcontract.repository.ProductARepository;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-
-
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BillingcontractApplication.class)
@@ -27,6 +30,9 @@ import org.springframework.web.context.WebApplicationContext;
 public class ProductAControllerTest {
 
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -40,11 +46,16 @@ public class ProductAControllerTest {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
     }
 
+    @After
+    public void tearDown() throws Exception{
+        productARepository.deleteAll();
+    }
+
     @Test
     public void getProductAByAccountIdTest() throws Exception{
         //given
         ProductA newProduct = new ProductA();
-        newProduct.setAccountId(1l);
+        newProduct.setAccountId(1L);
         newProduct.setContractLength(3);
 
         productARepository.save(newProduct);
@@ -54,8 +65,6 @@ public class ProductAControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.accountId", is(1)))
                 .andExpect(jsonPath("$.contractLength", is(3)));
-
-        productARepository.deleteAll();
     }
 
 
@@ -63,11 +72,11 @@ public class ProductAControllerTest {
     public void getAllProductATest() throws Exception{
         //given
         ProductA productA1 = new ProductA();
-        productA1.setAccountId(1l);
+        productA1.setAccountId(1L);
         productA1.setContractLength(3);
 
         ProductA productA2 = new ProductA();
-        productA2.setAccountId(2l);
+        productA2.setAccountId(2L);
         productA2.setContractLength(6);
 
         productARepository.save(productA1);
@@ -77,8 +86,22 @@ public class ProductAControllerTest {
         mockMvc.perform(get("/productA/")).andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].accountId", is(1)));
+    }
 
-        productARepository.deleteAll();
+    @Test
+    public void addProductATest() throws Exception{
+        //given
+        ProductA productA = new ProductA();
+        productA.setAccountId(1L);
+        productA.setContractLength(3);
+
+        //when + then
+        mockMvc.perform(post("/productA/").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(productA)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountId", is(1)));
+
     }
 
 }
