c42ef83ad1febe42eb4ac5ac126630b6dcde23bd
wesjordan <jordan.wesley@gmail.com>
2017-09-29 22:25:34 +0200

    format responses resulting in validation error

---
+++ src/main/java/com/wesjordan/billingcontract/validation/ApiValidationExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.wesjordan.billingcontract.validation;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ApiValidationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> invalidArgument(MethodArgumentNotValidException exception){
+        BindingResult bindingResult = exception.getBindingResult();
+
+        ExceptionResponse response = new ExceptionResponse();
+        response.setErrorCode("Validation error");
+        response.setErrorMessage("Invalid input");
+        response.setErrors(fromBindingErrors(bindingResult));
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    private List<String> fromBindingErrors(Errors errors){
+        List<String> errorList = new ArrayList<>();
+        for(ObjectError objectError: errors.getAllErrors()){
+            errorList.add(objectError.getCodes()[0]);
+        }
+        return errorList;
+    }
+}
