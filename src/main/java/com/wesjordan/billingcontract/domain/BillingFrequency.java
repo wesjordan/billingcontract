c4db141f34eb552ee6c4ddd492c856477629a32c
wesjordan <jordan.wesley@gmail.com>
2017-09-30 17:04:44 +0200

    enum to represent billing frequencies for products

---
+++ src/main/java/com/wesjordan/billingcontract/domain/BillingFrequency.java
@@ -0,0 +1,17 @@
+package com.wesjordan.billingcontract.domain;
+
+public enum BillingFrequency {
+
+    MONTHLY(1), QUARTERLY(3), ANNUALLY(12);
+
+    private Integer billingFrequency;
+
+    BillingFrequency(Integer billingFrequency){
+        this.billingFrequency = billingFrequency;
+    }
+
+    public Integer getBillingFrequency(){
+        return billingFrequency;
+    }
+
+}
