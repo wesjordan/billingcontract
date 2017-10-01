7557466bafd5513929765234fcc445780df26cff
wesjordan <jordan.wesley@gmail.com>
2017-10-01 20:49:25 +0200

    ++ meanbean for pojo testing

---
+++ src/test/java/com/wesjordan/billingcontract/domain/MeanBeanTest.java
@@ -0,0 +1,22 @@
+package com.wesjordan.billingcontract.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.meanbean.test.BeanTester;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class MeanBeanTest {
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void testProductA(){
+        BeanTester beanTester = new BeanTester();
+        beanTester.testBean(ProductA.class);
+    }
+}
