73ce4247ac641eedd74259d875a78c2aac0d7d99
wesjordan <jordan.wesley@gmail.com>
2017-10-02 22:15:19 +0200

    tests for Money object

--- src/test/java/com/wesjordan/billingcontract/domain/MeanBeanTest.java
+++ src/test/java/com/wesjordan/billingcontract/domain/MeanBeanTest.java
@@ -1,16 +1,44 @@
 package com.wesjordan.billingcontract.domain;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.meanbean.lang.Factory;
 import org.meanbean.test.BeanTester;
+import org.meanbean.test.Configuration;
+import org.meanbean.test.ConfigurationBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Currency;
 
 @RunWith(SpringRunner.class)
 public class MeanBeanTest {
+
+    @Before
+    public void setup(){
+
+    }
 
     @Test
     public void testProductA(){
         BeanTester beanTester = new BeanTester();
         beanTester.testBean(ProductA.class);
+        beanTester.testBean(Money.class);
+    }
+
+    @Test
+    public void testMoney(){
+        BeanTester beanTester = new BeanTester();
+        Configuration config = new ConfigurationBuilder().overrideFactory("currency", new CurrencyFactory()).build();
+        beanTester.testBean(Money.class, config);
+    }
+
+
+    private class CurrencyFactory implements Factory<Currency> {
+
+        @Override
+        public Currency create() {
+            return  Currency.getInstance("USD");
+        }
     }
 }
