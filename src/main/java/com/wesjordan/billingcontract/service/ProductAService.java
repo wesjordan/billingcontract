5d3481b8f63cc1fa8a0fc0323fc21d46768b33a7
wesjordan <jordan.wesley@gmail.com>
2017-11-01 20:26:16 +0200

    all write operations now persist event info to kafka

--- src/main/java/com/wesjordan/billingcontract/service/ProductAService.java
+++ src/main/java/com/wesjordan/billingcontract/service/ProductAService.java
@@ -1,30 +1,46 @@
 package com.wesjordan.billingcontract.service;
 
-import com.wesjordan.billingcontract.domain.ProductA;
+import com.wesjordan.billingcontract.dto.ProductADto;
+import com.wesjordan.billingcontract.mapping.ProductAMapper;
 import com.wesjordan.billingcontract.repository.ProductARepository;
+import com.wesjordan.billingcontract.stream.producer.ProductAProducer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ProductAService {
 
+    private static final Log logger = LogFactory.getLog(ProductAService.class);
+
     private ProductARepository productARepository;
+    private ProductAProducer productAProducer;
+    private ProductAMapper productAMapper;
 
     @Autowired
-    public ProductAService(ProductARepository productARepository){
+    public ProductAService(ProductARepository productARepository, ProductAProducer productAProducer, ProductAMapper productAMapper){
         this.productARepository = productARepository;
+        this.productAProducer = productAProducer;
+        this.productAMapper = productAMapper;
+    }
+
+    public void addProductA(ProductADto productADto){
+        productAProducer.publishProductACreatedEvent(productADto);
+    }
+
+    public void updateProductA(ProductADto productADto){
+        productAProducer.publishProductAUpdatedEvent(productADto);
+    }
+
+    public Iterable<ProductADto> getAllProducts(){
+        return productAMapper.mapAsList(productARepository.findAll(), ProductADto.class);
+    }
+
+    public ProductADto getProductByAccountId(Long accountId){
+        return productAMapper.map(productARepository.findByAccountId(accountId), ProductADto.class);
     }
 
 
-    public ProductA addProductA(ProductA productA){
-        return productARepository.save(productA);
-    }
-
-    public Iterable<ProductA> getAllProducts(){
-        return productARepository.findAll();
-    }
-
-    public ProductA getProductByAccountId(Long accountId){
-        return productARepository.findByAccountId(accountId);
-    }
 }
