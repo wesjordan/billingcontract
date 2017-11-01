199ace57a8c1b90f4003812ee6c9120bf8e686aa
wesjordan <jordan.wesley@gmail.com>
2017-11-01 20:28:54 +0200

    kafka configs

---
+++ src/main/java/com/wesjordan/billingcontract/stream/ProductAKafkaConfig.java
@@ -0,0 +1,40 @@
+package com.wesjordan.billingcontract.stream;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ProductAKafkaConfig {
+
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public KafkaTemplate<Integer, String> kafkaTemplate(){
+        return new KafkaTemplate<>(getProductAProducerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<Integer, String> getProductAProducerFactory(){
+        return new DefaultKafkaProducerFactory<>(productAProducerConfigs());
+    }
+
+    @Bean
+    public Map<String, Object> productAProducerConfigs(){
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);//TODO: get from config server
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return props;
+    }
+}
