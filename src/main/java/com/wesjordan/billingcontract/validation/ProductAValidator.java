e7bc26926cf75ebcbf9077c9f680777e098aebb4
wesjordan <jordan.wesley@gmail.com>
2017-11-01 20:30:05 +0200

    changed to validate dto object

--- src/main/java/com/wesjordan/billingcontract/validation/ProductAValidator.java
+++ src/main/java/com/wesjordan/billingcontract/validation/ProductAValidator.java
@@ -1,6 +1,6 @@
 package com.wesjordan.billingcontract.validation;
 
-import com.wesjordan.billingcontract.domain.ProductA;
+import com.wesjordan.billingcontract.dto.ProductADto;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -10,12 +10,12 @@ public class ProductAValidator implements Validator{
 
     @Override
     public boolean supports(Class<?> aClass) {
-        return ProductA.class.isAssignableFrom(aClass);
+        return ProductADto.class.isAssignableFrom(aClass);
     }
 
     @Override
     public void validate(Object o, Errors errors) {
-        ProductA productA = (ProductA)o;
+        ProductADto productA = (ProductADto) o;
         if(productA.getAccountId() == null || productA.getAccountId().equals(0L)){
             errors.rejectValue("accountId", "The supplied accountId is invalid");
         }
