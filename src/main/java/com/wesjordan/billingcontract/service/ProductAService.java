cc489618f9add2d1a126336b89a8e9b8e6df6162
wesjordan <jordan.wesley@gmail.com>
2017-11-02 19:59:31 +0200

    sonar: cleanup

--- src/main/java/com/wesjordan/billingcontract/service/ProductAService.java
+++ src/main/java/com/wesjordan/billingcontract/service/ProductAService.java
@@ -4,16 +4,12 @@ import com.wesjordan.billingcontract.dto.ProductADto;
 import com.wesjordan.billingcontract.mapping.ProductAMapper;
 import com.wesjordan.billingcontract.repository.ProductARepository;
 import com.wesjordan.billingcontract.stream.producer.ProductAProducer;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class ProductAService {
-
-    private static final Log logger = LogFactory.getLog(ProductAService.class);
 
     private ProductARepository productARepository;
     private ProductAProducer productAProducer;
