e777c7a90c5ffc34dac60b9f9ac603135ab9b3a5
wesjordan <jordan.wesley@gmail.com>
2017-11-06 21:50:42 +0200

    + no args constructor

--- src/main/java/com/wesjordan/billingcontract/stream/event/ProductAEventMessage.java
+++ src/main/java/com/wesjordan/billingcontract/stream/event/ProductAEventMessage.java
@@ -2,10 +2,12 @@ package com.wesjordan.billingcontract.stream.event;
 
 import com.wesjordan.billingcontract.dto.ProductADto;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ProductAEventMessage {
     private ProductAEventType event;
     private ProductADto payload;
