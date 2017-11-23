5147fe694220d3ec77d8d448a37a9f9089df6c5b
wesjordan <jordan.wesley@gmail.com>
2017-11-23 22:19:58 +0200

    externalised topic the consumer listened to added countdown latch (yet to figure out the need for it ... little info in spring kafka docs)

--- src/main/java/com/wesjordan/billingcontract/stream/consumer/ProductAConsumer.java
+++ src/main/java/com/wesjordan/billingcontract/stream/consumer/ProductAConsumer.java
@@ -4,10 +4,13 @@ import com.wesjordan.billingcontract.domain.ProductA;
 import com.wesjordan.billingcontract.mapping.ProductAMapper;
 import com.wesjordan.billingcontract.service.ProductAService;
 import com.wesjordan.billingcontract.stream.event.ProductAEventMessage;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
 
 @Slf4j
 @Component
@@ -16,13 +19,16 @@ public class ProductAConsumer {
     private ProductAService productAService;
     private ProductAMapper productAMapper;
 
+    @Getter
+    private CountDownLatch latch = new CountDownLatch(1);
+
     @Autowired
     public ProductAConsumer(ProductAService productAService, ProductAMapper productAMapper) {
         this.productAService = productAService;
         this.productAMapper = productAMapper;
     }
 
-    @KafkaListener(topics = "ProductA", groupId = "productAConsumerGroup")
+    @KafkaListener(topics = "${productA.topic.consume}")
     public void readProductAEvent(ProductAEventMessage message) {
 
         if (message != null) {
@@ -32,5 +38,7 @@ public class ProductAConsumer {
         } else {
             log.error("null message received from stream");
         }
+
+        latch.countDown();
     }
 }
