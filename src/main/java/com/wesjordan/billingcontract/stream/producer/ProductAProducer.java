567899b0444e92fd26846997acc0dd82b6b4e518
wesjordan <jordan.wesley@gmail.com>
2017-12-05 22:03:34 +0200

    spring-integration changes

--- src/main/java/com/wesjordan/billingcontract/stream/producer/ProductAProducer.java
+++ src/main/java/com/wesjordan/billingcontract/stream/producer/ProductAProducer.java
@@ -4,27 +4,21 @@ import com.wesjordan.billingcontract.dto.ProductADto;
 import com.wesjordan.billingcontract.stream.event.ProductAEventMessage;
 import com.wesjordan.billingcontract.stream.event.ProductAEventType;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Slf4j
 @Component
 public class ProductAProducer {
 
-    @Value("${productA.topic.publish}")
-    private String productAPublishTopic;
-
-    private KafkaTemplate<String, ProductAEventMessage> kafkaTemplate;
+    private DirectChannel producingChannel;
 
     @Autowired
-    public ProductAProducer(KafkaTemplate<String, ProductAEventMessage> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    public ProductAProducer(DirectChannel producingChannel) {
+        this.producingChannel = producingChannel;
     }
 
     public void publishProductACreatedEvent(ProductADto productADto){
@@ -38,20 +32,8 @@ public class ProductAProducer {
     }
 
     private void publish(ProductAEventMessage data) {
-        ProducerRecord<String, ProductAEventMessage> record = new ProducerRecord<>(productAPublishTopic, data);
-
-        ListenableFuture<SendResult<String, ProductAEventMessage>> future = kafkaTemplate.send(record);
-        future.addCallback(new ListenableFutureCallback<SendResult<String, ProductAEventMessage>>() {
-            @Override
-            public void onFailure(Throwable throwable) {
-                log.error(throwable.getMessage());
-            }
-
-            @Override
-            public void onSuccess(SendResult<String, ProductAEventMessage> productASendResult) {
-                log.info("event message written to kafka!");
-            }
-        });
-
+        Message m = MessageBuilder.withPayload(data).build();
+        producingChannel.send(m);
+        log.info("event message published to kafka!");
     }
 }
